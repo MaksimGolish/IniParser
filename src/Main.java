dc162cbde5de1a6209d653e766a4c02fb9334639
Max <maksim.golish2002@gmail.com>
2020-09-11 01:11:01 +0300

    Added getObject method

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.io.FileNotFoundException;
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         IniParser iniParser = new IniParser();
-        Ini ini = iniParser.parse(new File("/Users/maksimgolish/IdeaProjects/iniparser/src/test.ini"));
+        Ini ini = iniParser.parse(new File("src/test.ini"));
         System.out.println(ini.getString("COMMON", "DiscCachePath"));
         System.out.println(ini.getString("Section", "login"));
         System.out.println(ini.getFloat("COMMON", "LogNMCD"));
