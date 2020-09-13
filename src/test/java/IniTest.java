393b68a3a7141ec7c5791af388d2359925247dac
Max <maksim.golish2002@gmail.com>
2020-09-14 01:47:44 +0300

    added junit

---
+++ src/test/java/IniTest.java
@@ -0,0 +1,14 @@
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+public class IniTest {
+    @Test
+    public void testParsing() throws Exception {
+
+    }
+}
