cd7f6f52cce80522269db01a77a8ad9c09d11c89
MaksimGolish <maksim.golish2002@gmail.com>
2020-10-02 23:53:07 +0300

    fixed folder

---
+++ IniParser/src/test/java/IniTest.java
@@ -0,0 +1,14 @@
+import ini.model.Ini;
+import ini.model.Property;
+import ini.model.Section;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+public class IniTest {
+    @Test
+    public void 
+}
